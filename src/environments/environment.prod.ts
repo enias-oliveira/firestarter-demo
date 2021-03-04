20a260a83d2f63fd486d44a0cba0fa1ec54cc5a9
Enias Oliveira <eniasoliveira27@gmail.com>
2021-03-04 18:26:32 -0300

    feat: adds simple routing and firebase setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCgDM9Wsj0ncKnrABt53RmH0n9PV-30E24',
+    authDomain: 'firestarter-fea63.firebaseapp.com',
+    projectId: 'firestarter-fea63',
+    storageBucket: 'firestarter-fea63.appspot.com',
+    messagingSenderId: '259301470708',
+    appId: '1:259301470708:web:6e646281cc7eb7469b89a1',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCgDM9Wsj0ncKnrABt53RmH0n9PV-30E24',
+    authDomain: 'firestarter-fea63.firebaseapp.com',
+    projectId: 'firestarter-fea63',
+    storageBucket: 'firestarter-fea63.appspot.com',
+    messagingSenderId: '259301470708',
+    appId: '1:259301470708:web:6e646281cc7eb7469b89a1',
+  },
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
