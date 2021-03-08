b5db02d3850e5efa7de7c4a6a0986f4288de842a
Enias Oliveira <eniasoliveira27@gmail.com>
2021-03-07 23:16:14 -0300

    feat: adds login page, route and google login button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [{ path: '', component: HomePageComponent }];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./user/user.module').then((mod) => mod.UserModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
